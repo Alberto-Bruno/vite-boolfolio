c46e424c6b1a1452bd89b69ed22b395b14a07a6e
Alberto-Bruno <brunoalberto97@outlook.it>
2023-03-17 01:40:53 +0100

    add page add projects

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import AppHeader from './components/AppHeader.vue';
 export default {
     name: 'App',
-    components: { AppHeader }
+    components: { AppHeader },
 };
 </script>
 
---
+++ src/components/projects/ProjectList.vue
@@ -0,0 +1,16 @@
+<script >
+import ProjectCard from './ProjectCard.vue';
+export default {
+    name: 'ProjectList',
+    props: { projects: Array },
+    components: { ProjectCard }
+};
+</script>
+
+<template>
+    <div v-for="project in projects" :key="project.id" class="container">
+        <project-card :project="project"></project-card>
+    </div>
+</template>
+
+<style></style>
