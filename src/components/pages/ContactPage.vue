d40b385f3b704c6f21b0d16badba39a64b2a173f
Alberto-Bruno <brunoalberto97@outlook.it>
2023-03-17 02:45:31 +0100

    add pages

---
+++ src/components/pages/ContactPage.vue
@@ -0,0 +1,9 @@
+<script >
+export default {
+    name: 'ContactPages',
+};
+</script>
+
+<template></template>
+
+<style></style>
--- src/components/pages/HomePage.vue
+++ src/components/pages/HomePage.vue
@@ -1,9 +1,26 @@
 <script >
+import axios from 'axios';
+import ProjectList from '../projects/projectList.vue';
+const apiBaseUrl = 'http://localhost:8000/api/';
 export default {
-    name: '',
+    name: 'HomePage',
+    components: { ProjectList },
+    data() { return { projects: [], } },
+    methods: {
+        fetchProjects() {
+            axios.get(apiBaseUrl + 'projects').then(res => {
+                this.projects = res.data;
+            })
+        }
+    },
+    created() {
+        this.fetchProjects();
+    }
 };
 </script>
 
-<template></template>
+<template>
+    <projects-list :projects="projects"></projects-list>
+</template>
 
 <style></style>
